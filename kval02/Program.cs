4ea008f895a0bd3c08bdc36449ecf90acba3bbef
Анастасия Максимова <Anastasia3023@yandex.ru>
2023-07-03 16:42:20 +0300

    Добавьте файлы проекта.

---
+++ kval02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace kval02
+{
+    internal class Program
+    {
+       
+            static void Main(string[] args)
+            {
+           
+        }
+        }
+    }
+    
+
